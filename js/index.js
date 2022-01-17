5ace259d709a6dfea9718a95a299ea9c03e1a22e
AngelBorjaUceda <74968109+AngelBorjaUceda@users.noreply.github.com>
2022-01-17 08:03:30 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function ponerMenu(){
+  
+    let nombre=document.getElementById("name");
+    let liNombre = document.getElementById("liName");
+
+    
+    nombre.classList.add("d-none");
+    liNombre.classList.remove("d-none");
+
+      
+    
+}
